7e3071df24989e41f853840209f2791e1d7dea0f
Bui Minh Duc <ducbm0512@gmail.com>
2023-04-06 17:16:02 +0700

    [#11] Remove unused asset images
    Rename forgot_password_view to forgot_password_screen
    Update forgot_password_screen structure

---
+++ lib/ui/forgot_password/forgot_password_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:survey_flutter_ic/gen/assets.gen.dart';
+import 'package:survey_flutter_ic/ui/widget/input_field_widget.dart';
+import 'package:survey_flutter_ic/utils/dimension.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  const ForgotPasswordScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(Assets.images.bgLoginOverlay.path),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: Dimensions.paddingLarge),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image(
+                          image: Assets.images.icNimbleWhiteLogo.image().image,
+                          width: 168.0,
+                          height: 40.0,
+                        ),
+                        const SizedBox(height: 24.0),
+                        Text(
+                          AppLocalizations.of(context)
+                                  ?.forgotPasswordInstruction ??
+                              '',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyLarge
+                              ?.copyWith(color: Colors.white70),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 100.0),
+                      ]),
+                  const Padding(
+                      padding:
+                          EdgeInsets.only(bottom: Dimensions.paddingMedium),
+                      child: InputFieldWidget(
+                        key: Key('login_email'),
+                        textHint: 'Email',
+                        keyboardType: TextInputType.emailAddress,
+                        textInputAction: TextInputAction.next,
+                      )),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      minimumSize:
+                          const Size.fromHeight(Dimensions.buttonHeight),
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(Dimensions.radiusNormal),
+                      ),
+                    ),
+                    child: Text(
+                      AppLocalizations.of(context)?.forgotPasswordResetButton ??
+                          '',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleSmall
+                          ?.copyWith(color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+
+
+//   Widget _buildForgotPasswordView() {
+//     return Stack(
+//       children: [
+//         Image(
+//           image: Assets.images.nimbleBackground.image().image,
+//           fit: BoxFit.cover,
+//           width: double.infinity,
+//           height: double.infinity,
+//         ),
+//         Scaffold(
+//           backgroundColor: Colors.transparent,
+//           body: SafeArea(
+//             child: Container(
+//               padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 24),
+//               child: Column(
+//                 children: [],
+//               ),
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
