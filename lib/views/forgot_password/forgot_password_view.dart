2d22ddfff7701a3876430f09686a200f42550c80
Bui Minh Duc <ducbm0512@gmail.com>
2023-04-06 14:15:23 +0700

    [#11] Update localizable strings for English
    Update layout using Stack

--- lib/views/forgot_password/forgot_password_view.dart
+++ lib/views/forgot_password/forgot_password_view.dart
@@ -8,72 +8,30 @@ class ForgotPasswordView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          // Background image
-          Image(
-            image: Assets.images.nimbleLogo.image().image,
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: double.infinity,
-          ),
-          // Logo overlay
-          Positioned(
-            top: 80.0,
-            left: 20.0,
-            child: Image(
-              image: Assets.images.nimbleLogo.image().image,
-              width: 150.0,
-              height: 150.0,
-            ),
-          ),
-          // Description overlay
-          const Positioned(
-            top: 250.0,
-            left: 20.0,
-            right: 20.0,
-            child: Text(
-              'Please enter your email address below and we will send you a link to reset your password.',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18.0,
+    return _buildForgotPasswordView();
+  }
+
+  Widget _buildForgotPasswordView() {
+    return Stack(
+      children: [
+        Image(
+          image: Assets.images.nimbleBackground.image().image,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 24),
+              child: Column(
+                children: [],
               ),
-              textAlign: TextAlign.center,
             ),
           ),
-          // Email text field overlay
-          Positioned(
-            top: 350.0,
-            left: 20.0,
-            right: 20.0,
-            child: TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Email Address',
-                labelStyle: TextStyle(color: Colors.white),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-              ),
-              style: const TextStyle(color: Colors.white),
-              keyboardType: TextInputType.emailAddress,
-            ),
-          ),
-          // Reset button overlay
-          Positioned(
-            top: 450.0,
-            left: 20.0,
-            right: 20.0,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: const Text('Reset Password'),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
