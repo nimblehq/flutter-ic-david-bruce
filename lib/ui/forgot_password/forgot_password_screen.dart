caaec494ae13b0b7157d2ede1437e488ea43823c
David <duc@nimblehq.co>
2023-04-12 16:45:56 +0700

    [#11] Remove redundant code

--- lib/ui/forgot_password/forgot_password_screen.dart
+++ lib/ui/forgot_password/forgot_password_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
 import 'package:survey_flutter_ic/gen/assets.gen.dart';
 import 'package:survey_flutter_ic/ui/widget/input_field_widget.dart';
