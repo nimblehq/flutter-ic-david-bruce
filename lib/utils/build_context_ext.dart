1c2cf3297a06c058fe19a76b2a0fb0c453753080
David <duc@nimblehq.co>
2023-04-14 11:44:18 +0700

    [#21] Merge build_context_ext to context_ext

--- lib/utils/build_context_ext.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension BuildContextExtension on BuildContext {
-  void showMessageSnackBar({required String message}) =>
-      ScaffoldMessenger.of(this).showSnackBar(SnackBar(content: Text(message)));
-}
