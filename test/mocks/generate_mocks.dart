fe3714340b18f1ed6834379e60b5bdb31275f206
Bruce - Thien Nguyen <thien@nimblehq.co>
2023-04-20 18:55:41 +0700

    [#9] Add unit test

--- test/mocks/generate_mocks.dart
+++ test/mocks/generate_mocks.dart
@@ -5,8 +5,13 @@ import 'package:survey_flutter_ic/api/repository/auth_repository.dart';
 import 'package:survey_flutter_ic/usecases/base/base_use_case.dart';
 import 'package:survey_flutter_ic/usecases/login_use_case.dart';
 
-@GenerateMocks(
-    [AuthApiService, AuthRepository, DioError, LoginUseCase, UseCaseException])
+@GenerateMocks([
+  AuthApiService,
+  AuthRepository,
+  DioError,
+  LoginUseCase,
+  UseCaseException,
+])
 main() {
   // empty class to generate mock repository classes
 }
