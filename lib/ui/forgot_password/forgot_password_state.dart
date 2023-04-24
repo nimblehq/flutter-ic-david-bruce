5f39e8d92b5791dde59d8239a1e9ef21681d44d3
David <ducbm0512@gmail.com>
2023-04-24 08:38:51 +0700

    [13] Create ForgotPasswordState and ForgotPasswordViewModel

---
+++ lib/ui/forgot_password/forgot_password_state.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'forgot_password_state.freezed.dart';
+
+@freezed
+class ForgotPasswordState with _$ForgotPasswordState {
+  const factory ForgotPasswordState.init() = _Init;
+
+  const factory ForgotPasswordState.loading() = _Loading;
+
+  const factory ForgotPasswordState.success() = _Success;
+
+  const factory ForgotPasswordState.error(String errorMessage) = _Error;
+
+  const factory ForgotPasswordState.errorEmailInput() = _ErrorEmailInput;
+}
