10e8b45b0f02c0db56c0456d4b64b31d6eb20f42
David <ducbm0512@gmail.com>
2023-04-25 12:58:57 +0700

    [22] Improve code

--- lib/ui/forgot_password/forgot_password_screen.dart
+++ lib/ui/forgot_password/forgot_password_screen.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:survey_flutter_ic/gen/assets.gen.dart';
-import 'package:survey_flutter_ic/ui/widget/input_field_widget.dart';
-import 'package:survey_flutter_ic/utils/context_ext.dart';
-import 'package:survey_flutter_ic/utils/dimension.dart';
-import 'package:survey_flutter_ic/utils/custom_app_bar.dart';
-import 'package:survey_flutter_ic/utils/route_path.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:survey_flutter_ic/di/di.dart';
+import 'package:survey_flutter_ic/gen/assets.gen.dart';
 import 'package:survey_flutter_ic/ui/forgot_password/forgot_password_state.dart';
 import 'package:survey_flutter_ic/ui/forgot_password/forgot_password_view_model.dart';
+import 'package:survey_flutter_ic/ui/widget/input_field_widget.dart';
 import 'package:survey_flutter_ic/usecases/forgot_password_use_case.dart';
+import 'package:survey_flutter_ic/utils/context_ext.dart';
+import 'package:survey_flutter_ic/utils/custom_app_bar.dart';
+import 'package:survey_flutter_ic/utils/dimension.dart';
+import 'package:survey_flutter_ic/utils/route_path.dart';
 
 final forgotPasswordViewModelProvider = StateNotifierProvider.autoDispose<
     ForgotPasswordViewModel, ForgotPasswordState>(
