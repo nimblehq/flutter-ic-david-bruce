839df3d991b941903eaa1a3b7d5c68b106cbc189
David <ducbm0512@gmail.com>
2023-04-26 14:52:38 +0700

    [34] Implement Right Menu Slider using Drawer in Home Screen

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:survey_flutter_ic/utils/dimension.dart';
+import 'package:survey_flutter_ic/utils/context_ext.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,17 +7,73 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.symmetric(
-            horizontal: Dimensions.paddingLarge,
-          ),
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      endDrawer: Drawer(
+          backgroundColor: const Color(0xFF1E1E1E),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [Text('Home screen')],
-          ),
-        ),
+            children: [
+              Expanded(
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: [
+                    DrawerHeader(
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              'Mai',
+                              style: context.textTheme.headlineLarge?.copyWith(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          const CircleAvatar(
+                            backgroundColor: Colors.grey,
+                            child: Icon(
+                              Icons.person,
+                              size: 36.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Divider(
+                        color: Colors.white70,
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        'Logout',
+                        style: context.textTheme.bodyLarge
+                            ?.copyWith(color: Colors.white70),
+                      ),
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: ListTile(
+                  title: Text(
+                    'Version 1.0',
+                    style: context.textTheme.bodySmall
+                        ?.copyWith(color: Colors.white70),
+                  ),
+                ),
+              ),
+            ],
+          )),
+      body: const Center(
+        child: Text('Welcome to Home Screen!'),
       ),
     );
   }
