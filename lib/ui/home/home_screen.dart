4abdf8fdcfa9bcdffad2756ae042931643bbd399
David <ducbm0512@gmail.com>
2023-04-28 10:14:20 +0700

    [34] Improve code by separating menu into SideMenu

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:survey_flutter_ic/ui/home/home_side_menu.dart';
+import 'package:survey_flutter_ic/ui/home/home_side_menu_ui_model.dart';
 import 'package:survey_flutter_ic/utils/context_ext.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,68 +12,12 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Screen'),
       ),
-      endDrawer: Drawer(
-          backgroundColor: const Color(0xFF1E1E1E),
-          child: Column(
-            children: [
-              Expanded(
-                child: ListView(
-                  padding: EdgeInsets.zero,
-                  children: [
-                    DrawerHeader(
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Text(
-                              'Mai',
-                              style: context.textTheme.headlineLarge?.copyWith(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          const CircleAvatar(
-                            backgroundColor: Colors.grey,
-                            child: Icon(
-                              Icons.person,
-                              size: 36.0,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Divider(
-                        color: Colors.white70,
-                      ),
-                    ),
-                    ListTile(
-                      title: Text(
-                        context.localization.logout,
-                        style: context.textTheme.bodyLarge
-                            ?.copyWith(color: Colors.white70),
-                      ),
-                      onTap: () {},
-                    ),
-                  ],
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: ListTile(
-                  title: Text(
-                    'Version 1.0',
-                    style: context.textTheme.bodySmall
-                        ?.copyWith(color: Colors.white70),
-                  ),
-                ),
-              ),
-            ],
-          )),
+      endDrawer: SideMenu(
+        sideMenuUIModel: SideMenuUIModel(
+          name: 'Mai',
+          version: '',
+        ),
+      ),
       body: const Center(
         child: Text('Welcome to Home Screen!'),
       ),
