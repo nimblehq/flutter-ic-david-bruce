74ff82d646ed434b9911fc6822624f073d6e5d19
David <ducbm0512@gmail.com>
2023-05-16 16:38:56 +0700

    [#36] Improve code

--- lib/usecases/save_survey_submission_use_case.dart
+++ lib/usecases/save_survey_submission_use_case.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
-import 'dart:ffi';
 import 'package:injectable/injectable.dart';
 import 'package:survey_flutter_ic/api/repository/survey_repository.dart';
 import 'package:survey_flutter_ic/model/survey_submission_model.dart';
